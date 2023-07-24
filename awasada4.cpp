21e989b40f67c22785d51fd2e618b80bdd1ab155
pamawasada <139094561+pamawasada@users.noreply.github.com>
2023-07-24 10:06:04 +0700

    Add files via upload

---
+++ awasada4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int b;
+  cout <<"Input c++ Score : ";
+  cin >> b;
+  cout << "You "<<((b>=50)? " pas s" :"fail ")<< " with score " << b <<endl;
+  system("pause");
+  return(0);
+}
